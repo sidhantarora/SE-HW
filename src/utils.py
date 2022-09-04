c0086b53cbb0952ba657acec75e1f5e2689196ec
sravanth1 <sravanthreddy911@gmail.com>
2022-09-04 16:45:34 -0400

    added coerce function

---
+++ src/utils.py
@@ -0,0 +1,36 @@
+import re
+
+help='''
+    CSV : summarized csv file
+    (c) 2022 Tim Menzies <timm@ieee.org> BSD-2 license
+    USAGE: lua seen.lua [OPTIONS]
+    OPTIONS:
+    -e  --eg        start-up example                      = nothing
+    -d  --dump      on test failure, exit with stack dump = false
+    -f  --file      file with csv data                    = ../data/auto93.csv
+    -h  --help      show help                             = false
+    -n  --nums      number of nums to keep                = 512
+    -s  --seed      random number seed                    = 10019
+    -S  --seperator feild seperator                       = ,
+'''  
+
+
+def coerce(s):
+    def fun(s1):
+        if s1=="true":
+            return True
+        if s1=="false":
+            return False
+        return s1
+    try:
+        return int(s) or float(s) or fun(re.match(s, "^%s*(.−)%s*$"))
+    except RuntimeError as e:
+        return None
+
+def function(k, x):
+    the[k] = coerce(x)
+    return the[k]
+
+the = {}
+k, x = re.sub(help, "\n [−][%S]+[%s]+[−][−]([%S]+)[^\n]+= ([%S]+)")
+function(k, x)
