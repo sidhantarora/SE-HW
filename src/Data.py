b8b1f1581d72745937df0c2a2ffc026bd47bb4a8
Vinita Ramnani <vjramnan@ncsu.edu>
2022-09-11 01:45:45 -0400

    added stats

--- src/Data.py
+++ src/Data.py
@@ -1,5 +1,6 @@
 import csv
 import Cols
+import rnd
 
 class Data:
     def __init__(self, src) -> None:
@@ -18,3 +19,20 @@ class Data:
         for todo in self.cols.data:
             for col in todo:
                 col.add(row.cells[col.at])
+
+    def stats(self, places, showCols, func):
+        sc = None
+        if showCols:
+            sc = showCols
+        else:
+            sc = self.cols.y
+        t = dict()
+        x = False
+        for col in sc:
+            v = func(col)
+            if type(v) == type(1) and rnd(v, places):
+                x = True
+                t[col.name] = x
+            else:
+                t[col.name] = v
+        return t
