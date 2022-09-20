29bad241a4710fbdc64413ea56617bbec83cce96
Akash Sarda <aksarda@ncsu.edu>
2022-09-19 20:05:47 -0400

    added python files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='SEHW',
+    version='0.1dev',
+    packages=['src', ],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ]
+)
